a7378e5b506e2c95b748df6c7dd7f7b602aeabcf
David Zhang <david.zhang@wishabi.com>
2015-09-27 09:51:39 -0400

    removed commas from price string

--- server/scripts/scrap.rb
+++ server/scripts/scrap.rb
@@ -9,7 +9,7 @@ require 'open-uri'
 require 'json'
 node = Nokogiri.HTML(open(url))
 
-price = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-2']/div[@class='col-1']/div[@id='itemdetails']/div[@class='col-2']/table[@class='ad-attributes']/tr[2]/td/div/span/strong").text()
+price = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-2']/div[@class='col-1']/div[@id='itemdetails']/div[@class='col-2']/table[@class='ad-attributes']/tr[2]/td/div/span/strong").text().gsub(',','')
 date = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-2']/div[@class='col-1']/div[@id='itemdetails']/div[@class='col-2']/table[@class='ad-attributes']/tr[1]/td").text()
 addr = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-2']/div[@class='col-1']/div[@id='itemdetails']/div[@class='col-2']/table[@class='ad-attributes']/tr[3]/td").text().split(/\n/)[0]
 name = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-0']/div[3]/span/h1").text()
