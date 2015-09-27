d1016f0c918aa5ea6bdfc41c4fa6c0b7737c500f
David Zhang <david.zhang@wishabi.com>
2015-09-27 02:33:52 -0400

    Fixed parsing issue

--- server/scripts/scrap.rb
+++ server/scripts/scrap.rb
@@ -11,9 +11,9 @@ response = HTTParty.get(url)
 node = Nokogiri.HTML(response.parsed_response)
 
 price = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-2']/div[@class='col-1']/div[@id='itemdetails']/div[@class='col-2']/table[@class='ad-attributes']/tr[2]/td/div/span/strong").text()
-date = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-2']/div[@class='col-1']/div[@id='itemdetails']/div[@class='col-2']/table[@class='ad-attributes']/tbody/tr[1]/td").text()
-addr = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-2']/div[@class='col-1']/div[@id='itemdetails']/div[@class='col-2']/table[@class='ad-attributes']/tr[3]/td").text()
-name = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-0']/div[3]/span/h1")
+date = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-2']/div[@class='col-1']/div[@id='itemdetails']/div[@class='col-2']/table[@class='ad-attributes']/tr[1]/td").text()
+addr = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-2']/div[@class='col-1']/div[@id='itemdetails']/div[@class='col-2']/table[@class='ad-attributes']/tr[3]/td").text().split(/\n/)[0]
+name = node.xpath("/html/body[@id='PageVIP']/div[@id='MainContainer']/div[@class='layout-0']/div[3]/span/h1").text()
 
 json = {
   :price => price,
